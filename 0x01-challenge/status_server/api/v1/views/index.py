b310fea0c42a96c1cad645e41bf5e0f8c538a39a
Wangui <lizkim685@gmail.com>
2022-09-28 14:56:54 +0300

    Fix my code challenge.

---
+++ 0x01-challenge/status_server/api/v1/views/index.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Index view
+"""
+from api.v1.views import app_views
+from flask import jsonify, request
+
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    """ Status of the web server
+    """
+    return jsonify({"status": "OK"})
+    
